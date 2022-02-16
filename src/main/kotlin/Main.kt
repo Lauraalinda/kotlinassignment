61731f6ec35aadbb1a6a20b3e9ebe0c93fb33b29
Lauraalinda <lauraallindaa@gmail.com>
2022-02-16 07:07:44 +0300

    My first application

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var studentName:String ="Kengaaju_Laura_Alinda"
+    var age=21
+    var phoneNumber:String="0756046099"
+    var weight_in_kg=65
+    var kenyan:Boolean=false
+
+
+    println(studentName)
+    println(age)
+    println(phoneNumber)
+    println(weight_in_kg)
+    println(kenyan)
+}
